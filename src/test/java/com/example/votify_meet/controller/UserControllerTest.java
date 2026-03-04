34aa667997e40008e0e6157e168e6700daaf046c
buse <Buse.krgl20@gmail.com>
2026-03-04 14:34:51 +0300

    (test): test for pre-push script

--- src/test/java/com/example/votify_meet/controller/UserControllerTest.java
+++ src/test/java/com/example/votify_meet/controller/UserControllerTest.java
@@ -51,7 +51,7 @@ public class UserControllerTest {
     @DisplayName("Successful User Creation - 201 Created should returned")
     void createUser_returns201_whenValidRequest() throws Exception {
         // Arrange
-        UsersRequestDto request = new UsersRequestDto(FIRST_NAME, LAST_NAME, EMAIL, "145*9o");
+        UsersRequestDto request = new UsersRequestDto(FIRST_NAME, LAST_NAME, EMAIL, "");
 
         // Act
         when(usersService.createUser(any(UsersRequestDto.class))).thenReturn(standardResponse);
