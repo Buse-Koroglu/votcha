a4ae95513df5bb7e201b144c1fc2696831659f82
buse <Buse.krgl20@gmail.com>
2026-03-04 15:27:36 +0300

    (test): testing pre-push in true case

--- src/test/java/com/example/votify_meet/controller/UserControllerTest.java
+++ src/test/java/com/example/votify_meet/controller/UserControllerTest.java
@@ -50,7 +50,7 @@ public class UserControllerTest {
     @DisplayName("Successful User Creation - 201 Created should returned")
     void createUser_returns201_whenValidRequest() throws Exception {
         // Arrange
-        UsersRequestDto request = new UsersRequestDto(FIRST_NAME, LAST_NAME, EMAIL, " ");
+        UsersRequestDto request = new UsersRequestDto(FIRST_NAME, LAST_NAME, EMAIL, "145*9o");
 
         // Act
         when(usersService.createUser(any(UsersRequestDto.class))).thenReturn(standardResponse);
