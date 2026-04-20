d586eca9eeae5ea2761036246b0a5e59bef8fac3
enes uluc <enesuluc04@gmail.com>
2026-04-20 14:21:09 +0300

    fix: UsersServiceTest fixed, ApplicationEventPublisher is mocked

--- src/test/java/com/example/votcha/service/UserServiceTest.java
+++ src/test/java/com/example/votcha/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.votcha.service;
 
+import com.example.votcha.common.event.UserCreatedSyncEvent;
 import com.example.votcha.users.api.dto.UpdateUsersRequestDto;
 import com.example.votcha.users.api.dto.UsersResponseDto;
 import com.example.votcha.users.api.mapper.UsersMapper;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.Optional;
 
@@ -26,6 +28,9 @@ public class UserServiceTest {
 
     @Mock
     private UsersMapper usersMapper;
+
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @InjectMocks
     private UsersService  usersService;
@@ -50,7 +55,7 @@ public class UserServiceTest {
         willAnswer(invocation -> {
             Users userToUpdate = invocation.getArgument(1); // 2. parametre existingUser
             userToUpdate.setFirstName("Kate");
-            return null; // void metot olduğu için null dönüyoruz
+            return null; // return null because of void method
         }).given(usersMapper).update(request, existingUser);
 
         // Act
@@ -58,6 +63,9 @@ public class UserServiceTest {
 
         // Act
         UsersResponseDto actualResponse = usersService.patchUser(userId, request);
+
+
+
 
         // Assert
         assertThat(actualResponse.firstName()).isEqualTo("Kate");
