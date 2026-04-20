1e88f06653aed0bb18b7f49fc2d863a775ae6abf
enes uluc <enesuluc04@gmail.com>
2026-04-20 14:33:55 +0300

    fix: example ca.crt is added

--- src/test/java/com/example/votcha/VotchaApplicationTests.java
+++ src/test/java/com/example/votcha/VotchaApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.votcha;
 
+import com.example.votcha.votcha_search.domain.repository.UserElasticRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class VotchaApplicationTests {
+
+    @MockitoBean
+    private UserElasticRepository userElasticRepository;
 
 	@Test
 	void contextLoads() {
